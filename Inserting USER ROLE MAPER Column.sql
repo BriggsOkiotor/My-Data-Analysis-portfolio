258c184bbd82dcd7d8f5fca7d475dba80b505934
BRIGGS OKIOTOR <108902579+BriggsOkiotor@users.noreply.github.com>
2022-07-17 18:57:42 +0100

    Add files via upload

---
+++ Inserting USER ROLE MAPER Column.sql
@@ -0,0 +1,17 @@
+/*insert into  UserRoleMapper 
+(CreatedDate, Modified, Active, Deleted, CreatedBy, ModifiedBY, RoleID, PriviledgeID, DepartmentID, StaffID)
+values ('2019-12-01', GETDATE(), 1, 0, 5, 3, 26, 3, 5, 7)
+
+insert into  UserRoleMapper 
+(CreatedDate, Modified, Active, Deleted, CreatedBy, ModifiedBY, RoleID, PriviledgeID, DepartmentID, StaffID)
+values ('2003-11-01', GETDATE(), 1, 0, 5, 3, 26, 3, 5, 7)
+
+insert into  UserRoleMapper 
+(CreatedDate, Modified, Active, Deleted, CreatedBy, ModifiedBY, RoleID, PriviledgeID, DepartmentID, StaffID)
+values ('2003-12-21', GETDATE(), 1, 0, 5, 3, 26, 3, 5, 7)
+*/
+
+
+select * from [dbo].[UserRoleMapper]
+
+/*Successful*/
